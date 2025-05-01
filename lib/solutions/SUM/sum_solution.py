f14d08950fcd63a81e41e8f0f47965ed27f94e65
sbx_user1051 <sbx_user1051@169.254.14.217>
2025-05-01 14:28:28 +0000

    Thu May 01 14:28:28 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,11 @@
-
 class SumSolution:
-    
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, x: int, y: int) -> int:
+        """Return the sume of two integrer between a minimum and maximum range."""
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise TypeError("Both arguments must be integers.")
+        if x < 0 or y < 0:
+            raise ValueError("Both arguments must be non-negative.")
+        if x > 100 or y > 100:
+            raise ValueError("Both arguments must be less than or equal to 100.")
+        return x + y
+
