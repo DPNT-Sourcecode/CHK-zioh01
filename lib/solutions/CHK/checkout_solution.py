952c4c5bc8d165250efbff4ee37ffd73c512316f
sbx_user1051 <sbx_user1051@169.254.4.193>
2025-05-03 12:38:05 +0000

    Sat May 03 12:38:05 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,10 @@
 class CheckoutSolution:
+    def __init__(self):
+        # Set the prices table for SKUs
+        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+        # Set the special offers for SKUs
+
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+
